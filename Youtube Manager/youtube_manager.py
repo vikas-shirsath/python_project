612530d2cb49cc3f69c31e7060096cc6e66fcc26
vikas-shirsath <vikasshirsath04@gmail.com>
2024-07-22 13:57:08 +0530

    Project completion

--- Youtube Manager/youtube_manager.py
+++ Youtube Manager/youtube_manager.py
@@ -1,19 +1,53 @@
+import json 
 
+def load_data() :
+    try : 
+        with open( 'youtube.txt', 'r' ) as file : 
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos) :
+    with open('youtube.txt', 'w') as file :
+        json.dump(videos, file)
 
 def list_all_videos(videos):
-    pass
-
+    print("\n")
+    print("#" * 100)
+    for index, video in enumerate(videos, start=1) :
+        print(f"{index}. Name : {video['name']}, Duration : {video['time']}")
+    print("\n")
+    print("#" * 100)
+        
 def add_video(videos) :
-    pass
+    name = input("Enter video name : ")
+    time = input("Enter video duration : ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
 def update_video(videos) :
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter video Number to update from above list: "))
+    if 1 <= index <= len(videos) :
+        name = input("Enter the new video name : ")
+        time = input("Enter the new video Duration : ")
+        videos[index-1] = {'name':name, 'time':time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
 def delete_video(videos) :
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video no. to be delete : "))
+    
+    if 1<= index <= len(videos) :
+        del videos[index-1]
+        save_data_helper(videos)
+    else :
+        print("Invalide video index selected")
 
 def main() :
-    videos = []
+    videos = load_data()
 
     while True :
         print("\n Youtube Manager | Choose an option ")
@@ -23,7 +57,7 @@ def main() :
         print("4. Delete a youtube video ")
         print("5. Exit the app ")
         choice = input("Enter your choice :- ")
-
+        
         match choice :
             case '1' :
                 list_all_videos(videos)
