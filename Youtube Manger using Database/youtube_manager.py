cbb087fa88b178d1cca30d823aad32eb5b18d673
vikas-shirsath <vikasshirsath04@gmail.com>
2024-07-25 17:59:25 +0530

    completion of project

--- Youtube Manger using Database/youtube_manager.py
+++ Youtube Manger using Database/youtube_manager.py
@@ -13,23 +13,31 @@ cursor.execute('''
 ''')
 
 def list_videos():
-    pass
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+        print(row)
 
 def add_video():
     name = input("Enter Video Name : ")
     time = input("Enter the video Duration : ")
-
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?,?)", (name, time))
+    conn.commit()
+    
 def update_video():
     video_id = input("Enter Video Number to update video : ")
-    name = input("Enter Video Name : ")
-    time = input("Enter the video Duration : ")
+    new_name = input("Enter Video Name : ")
+    new_time = input("Enter the video Duration : ")
+    cursor.execute("UPDATE videos  SET name = ?, time = ?, WHERE id = ?",(new_name, new_time, video_id))
+    conn.commit()
     
 def delete_video():
     video_id = input("Enter Video Number to update video : ")
+    cursor.execute("DELETE FROM videos WHERE id = ?",(video_id,))
+    conn.commit()
 
 def main() :
     while(True):
-        print("\nYoutube Manager app with DB")
+        print("\nYoutube Manager app with DB || Choose an option ")
         print("1. List Videos")
         print("2. Add Videos ")
         print("3. Update Videos")
@@ -50,6 +58,8 @@ def main() :
                 break
             case _ :
                 print("Invalid Choice")
+    
+    conn.close()
 
 if __name__ == "__main__" :
     main()
